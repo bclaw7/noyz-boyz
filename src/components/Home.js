9cdaaab3a99ef4efbde6d677b32099e7a3b2f517
Byron Cole Lawlis <bclaw8300@gmail.com>
2021-08-29 14:26:45 -0600

    added icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
+import { Container, Image, Button } from 'react-bootstrap';
 import concert from '../images/awesome-sound-concert-hands.jpg';
 
 export default function Home() {
   return (
-    <>
-      <div
-        style={{
-          backgroundImage: `url(${concert})`,
-          backgroundSize: 'cover',
-        }}
-        className='py-5'>
-        <Container className='text-center py-5'>
-          <Image src='../background.png' fluid />
-        </Container>
-      </div>
-    </>
+    <div
+      style={{
+        backgroundImage: `url(${concert})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+      }}
+      className='py-5'>
+      <Container className='text-center py-4'>
+        <Image src='../logo.png' fluid />
+      </Container>
+      <Container className='text-center pt-5 px-5 d-grid mx-auto'>
+        <Button
+          href='mailto:bclaw.design@gmail.com'
+          variant='outline-info'
+          size='lg'
+          className='fs-2'>
+          The Best Sound
+        </Button>
+      </Container>
+    </div>
   );
 }
